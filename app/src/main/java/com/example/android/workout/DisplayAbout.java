6534a76842601971ea2de5bc71dc75dfe1a0919b
Aaron Haslam <aaronhaslam2@gmail.com>
2018-03-21 15:07:38 -0600

    Just testing

--- app/src/main/java/com/example/android/workout/DisplayAbout.java
+++ app/src/main/java/com/example/android/workout/DisplayAbout.java
@@ -2,8 +2,11 @@ package com.example.android.workout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayAbout extends AppCompatActivity {
+    TextView aboutTitle;
+    TextView aboutDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
