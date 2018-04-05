789ffb94af3da796c6b19a92d4a85823465189e4
Kent <le6790@gmail.com>
2018-04-05 17:22:11 -0600

    Added some Buttons and images to cardio program

--- app/src/main/java/com/example/android/workout/Cardio.java
+++ app/src/main/java/com/example/android/workout/Cardio.java
@@ -1,13 +1,36 @@
 package com.example.android.workout;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Cardio extends AppCompatActivity {
+    private Button cardioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardio);
+
+        cardioButton = findViewById(R.id.cardio_Button);
+
+
+        Drawable jogImage = getResources().getDrawable(R.drawable.jogging_cardio);
+        cardioButton.setBackgroundDrawable(jogImage);
+        cardioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCardio(view);
+            }
+        });
+    }
+
+    public final void openCardio(View view){
+        Intent intent = new Intent(this,DisplayAbout.class);
+        startActivity(intent);
     }
 }
