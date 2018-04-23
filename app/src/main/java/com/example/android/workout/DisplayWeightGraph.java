76cdecb5665d06cf8f6834309b911c2842bf77c1
Aaron Haslam <aaron.hasla@aggiemail.usu.edu>
2018-04-22 21:38:22 -0600

    fixed spacing and added a dynamic way to add data to graph

--- app/src/main/java/com/example/android/workout/DisplayWeightGraph.java
+++ app/src/main/java/com/example/android/workout/DisplayWeightGraph.java
@@ -15,60 +15,91 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class DisplayWeightGraph extends AppCompatActivity {
 
+
+
     Button calculateBMI;
+    Button addDataPoint;
     Double weight = 0.0;
     Double height = 0.0;
     Double BMI = 0.0;
 
     LineGraphSeries<DataPoint> series;
+    ArrayList<Double> weights = new ArrayList<Double>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_weight_graph);
 
-        initGraph();
-
         calculateBMI = findViewById(R.id.button_CalculateBMI);
+        addDataPoint = findViewById(R.id.button_AddDataPoint);
 
+        addDataPoint.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                AddDataPoint(v);
+            }
+        });
         calculateBMI.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Code here executes on main thread after user presses button
                 EnterUserHeight(v);
             }
         });
     }
 
     private void initGraph(){
-        int dataPoints = 1000;
+        int dataPoints = weights.size();
         double x,y;
         x=0;
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
         series = new LineGraphSeries<DataPoint>();
-        for(int i = 0; i<dataPoints; i++){
-            x = x + 0.01;
-            y = Math.sin(x);
+        for(Double i: weights){
+            x = x + 1;
+            y = i;
             series.appendData(new DataPoint(x,y),true,dataPoints);
         }
         graph.addSeries(series);
     }
 
+    public final void AddDataPoint(final View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add Data Point");
+        final EditText input = new EditText(this);
+        builder.setMessage("Please enter your weight in lbs.");
+        input.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        builder.setView(input);
+        builder.setPositiveButton("NEXT", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                weights.add(Double.parseDouble(input.getText().toString()));
+                initGraph();
+            }
+        });
+        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
     public final void EnterUserHeight(final View view){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Height");
         final EditText input = new EditText(this);
         builder.setMessage("Please enter your height in inches.");
-        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
         builder.setView(input);
         builder.setPositiveButton("NEXT", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                if(input.getText().toString()!= null){
-                    height = Double.parseDouble(input.getText().toString());
-                }
+                height = Double.parseDouble(input.getText().toString());
                 EnterUserWeight(view);
             }
         });
@@ -86,14 +117,12 @@ public class DisplayWeightGraph extends AppCompatActivity {
         builder.setTitle("Weight");
         final EditText input = new EditText(this);
         builder.setMessage("Please enter your weight in lbs.");
-        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
         builder.setView(input);
         builder.setPositiveButton("NEXT", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                if(input.getText().toString()!=null){
-                    weight = Double.parseDouble(input.getText().toString());
-                }
+                weight = Double.parseDouble(input.getText().toString());
                 calculateBMI();
             }
         });
